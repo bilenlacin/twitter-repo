4b36aec8cb49f8c18374ecdb97b0be7b577c4f6b
ONUR LACIN <bilenlacin@gmail.com>
2021-04-14 22:18:55 +0300

    left column were created

--- react/src/containers/HomePage.js
+++ react/src/containers/HomePage.js
@@ -11,122 +11,108 @@ class HomePage extends React.Component {
         <div class='row'>
           <div class='col-3'>
             <div className='navdiv'>
-              <nav className='navbar'>
+              <nav className='navbarr'>
                 <a href='TwitterBird'>
-                  <img src='icons/twitterbird.svg' width='30' alt='' />
+                  <img src='icons/twitterbird.svg' width='28' alt='' />
                 </a>
               </nav>
-              <nav class='navbar bg-light'>
-                <ul class='navbar-nav'>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='/home'>
-                      <div className='nav-img-text'>
-                        <div className='nav-img'>
-                          <img src='icons/home.svg' width='25' alt='' />
-                        </div>
-                        <div className='nav-text'>
-                          <span>Anasayfa</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='/explore'>
-                      <div className='nav-img-text'>
-                        <div className='nav-img'>
-                          <img src='icons/hash.svg' width='25' alt='' />
-                        </div>
-                        <div className='nav-text'>
-                          <span>Keşfet</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='/notifications'>
-                      <div className='nav-img-text'>
-                        <div className='nav-img'>
-                          <img src='icons/bell.svg' width='25' alt='' />
-                        </div>
-                        <div className='nav-text'>
-                          <span>Bildirimler</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='/messages'>
-                      <div className='nav-img-text'>
-                        <div className='nav-img'>
-                          <img src='icons/mail.svg' width='25' alt='' />
-                        </div>
-                        <div className='nav-text'>
-                          <span>Mesajlar</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='/bookmark'>
-                      <div className='nav-img-text'>
-                        <div className='nav-img'>
-                          <img src='icons/bookmark.svg' width='25' alt='' />
-                        </div>
-                        <div className='nav-text'>
-                          <span>Yer işaretleri</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='/lists'>
-                      <div className='nav-img-text'>
-                        <div className='nav-img'>
-                          <img src='icons/list.svg' width='25' alt='' />
-                        </div>
-                        <div className='nav-text'>
-                          <span>Listeler</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='/profile'>
-                      <div className='nav-img-text'>
-                        <div className='nav-img'>
-                          <img src='icons/person.svg' width='25' alt='' />
-                        </div>
-                        <div className='nav-text'>
-                          <span>Profil</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='/more'>
-                      <div className='nav-img-text'>
-                        <div className='nav-img'>
-                          <img src='icons/threedot.svg' width='25' alt='' />
-                        </div>
-                        <div className='nav-text'>
-                          <span>Daha fazla</span>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-                  <li class='nav-item'>
-                    <a class='nav-link' href='/twwet'>
-                      <div className='tweetSpan'>
-                        <span class='tweetspantext'>Tweetle</span>
-                      </div>
-                    </a>
-                  </li>
-                </ul>
+              <nav class='navbar-nav'>
+                <a class='navlink' href='/home'>
+                  <div className='nav-img-text'>
+                    <div className='nav-img'>
+                      <img src='icons/home.svg' width='22' alt='' />
+                    </div>
+                    <div className='nav-text'>Anasayfa</div>
+                  </div>
+                </a>
+
+                <a class='navlink' href='/explore'>
+                  <div className='nav-img-text'>
+                    <div className='nav-img'>
+                      <img src='icons/hash.svg' width='22' alt='' />
+                    </div>
+                    <div className='nav-text'>
+                      <span>Keşfet</span>
+                    </div>
+                  </div>
+                </a>
+
+                <a class='navlink' href='/notifications'>
+                  <div className='nav-img-text'>
+                    <div className='nav-img'>
+                      <img src='icons/bell.svg' width='22' alt='' />
+                    </div>
+                    <div className='nav-text'>
+                      <span>Bildirimler</span>
+                    </div>
+                  </div>
+                </a>
+
+                <a class='navlink' href='/messages'>
+                  <div className='nav-img-text'>
+                    <div className='nav-img'>
+                      <img src='icons/mail.svg' width='22' alt='' />
+                    </div>
+                    <div className='nav-text'>
+                      <span>Mesajlar</span>
+                    </div>
+                  </div>
+                </a>
+
+                <a class='navlink' href='/bookmark'>
+                  <div className='nav-img-text'>
+                    <div className='nav-img'>
+                      <img src='icons/bookmark.svg' width='19' alt='' />
+                    </div>
+                    <div className='nav-text'>
+                      <span>Yer İşaretleri</span>
+                    </div>
+                  </div>
+                </a>
+
+                <a class='navlink' href='/lists'>
+                  <div className='nav-img-text'>
+                    <div className='nav-img'>
+                      <img src='icons/list.svg' width='22' alt='' />
+                    </div>
+                    <div className='nav-text'>
+                      <span>Listeler</span>
+                    </div>
+                  </div>
+                </a>
+
+                <a class='navlink' href='/profile'>
+                  <div className='nav-img-text'>
+                    <div className='nav-img'>
+                      <img src='icons/person.svg' width='22' alt='' />
+                    </div>
+                    <div className='nav-text'>
+                      <span>Profil</span>
+                    </div>
+                  </div>
+                </a>
+
+                <a class='navlink' href='/more'>
+                  <div className='nav-img-text'>
+                    <div className='nav-img'>
+                      <img src='icons/threedot.svg' width='22' alt='' />
+                    </div>
+                    <div className='nav-text'>
+                      <span>Daha fazla</span>
+                    </div>
+                  </div>
+                </a>
+                <button class='navlinkt' href='/tweet'>
+                  Tweetle
+                </button>
               </nav>
               <button type='button' class='btn btn-xlg'>
                 <img src='icons/person.svg' width='25' alt='' />
-                <span> </span>
-                user
+                <div className='user'>
+                  <div className='userName'>User</div>
+                  <div className='userEmail'>@User</div>
+                </div>
+                <img class='more' src='icons/threedot.svg' width='20' alt='' />
               </button>
             </div>
           </div>
@@ -546,7 +532,7 @@ class HomePage extends React.Component {
               </div>
             </div>
           </div>
-          <div class='col'>
+          <div class='col-3'>
             <div className='search'>
               <nav class='navbar navbar-light bg-light'>
                 <form class='form-inline'>
